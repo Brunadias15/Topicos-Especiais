77c360a5a605f2459adfcd0124a4aacf7fca34e9
Bruna Dias <61753234+Brunadias15@users.noreply.github.com>
2022-11-28 21:26:19 -0400

    Add files via upload

---
+++ src/topicosavancados/TopicosAvancados.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package topicosavancados;
+
+/**
+ *
+ * @author Wesley
+ */
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+import java.io.File;
+import javax.swing.JFileChooser;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.Prefs;
+import ij.process.ImageConverter;
+import ij.process.ImageProcessor;
+import java.io.File;
+
+public class TopicosAvancados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File escolherArquivo = escolherArquivo();
+        String path = escolherArquivo.getAbsolutePath();
+
+        ImagePlus image = new ImagePlus(path);
+        ImageProcessor ip = image.getProcessor();
+        ImagePlus original = new ImagePlus("Imagem Original", ip);
+        original.show();
+
+        ImagePlus imgGrayscale = image.duplicate();
+        ImageConverter converterGrayscale = new ImageConverter(imgGrayscale);
+        converterGrayscale.convertToGray8();//convertendo a imagem para tons de cinza
+        IJ.run(imgGrayscale, "8-bit", "");
+        ImageProcessor ipGrayscale = imgGrayscale.getProcessor().duplicate();
+
+        System.out.println("Largura:" + imgGrayscale.getWidth());
+        System.out.println("Altura:" + imgGrayscale.getHeight());
+        System.out.println("Valores de pixels:");
+
+        String nPixels = "";
+        int [] histograma = new int[256];
+        for (int i=0;i<256;i++){
+            histograma[i]=0;
+        }
+        for (int linha = 0; linha < imgGrayscale.getWidth(); linha++) {
+            for (int coluna = 0; coluna < imgGrayscale.getHeight(); coluna++) {
+
+               histograma[ipGrayscale.getPixel(linha, coluna)]++;
+                       //nPixels += +ipGrayscale.getPixel(linha, coluna) + "\n";
+
+            }
+        }
+        for (int i=0; i<256;i++){
+            System.out.println(i+" Qtd "+histograma[i]);
+        }
+        System.out.println(nPixels);
+        imgGrayscale.show();
+
+    }
+
+    public static File escolherArquivo() {
+        JFileChooser file = new JFileChooser();
+        file.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int i = file.showSaveDialog(null);
+        if (i == 1) {
+
+        } else {
+            File arquivo = file.getSelectedFile();
+            return arquivo;
+        }
+        return null;
+    }
+
+}
